48c143675230e38c2e221d8616d5a2977b71c004
ChaseWelsh <fseusb@gmail.com>
2022-09-26 13:35:17 -0600

    commit tree d29b266737ec4332e888317b83dde2aae51728fb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,22 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    songs.first
+  end
+
+  # return the genre of the artist's first saved song
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    get_first_song.genre
   end
 
+  # return the number of songs associated with the artist
   def song_count
-    #return the number of songs associated with the artist
+    songs.length
   end
 
+  # return the number of genres associated with the artist
   def genre_count
-    #return the number of genres associated with the artist
+    songs.map(&:get_genre_name).uniq.length
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,15 +2,18 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
+  # return the number of songs in a genre
   def song_count
-    # return the number of songs in a genre
+    songs.length
   end
 
+  # return the number of artists associated with the genre
   def artist_count
-    # return the number of artists associated with the genre
+    artists.count
   end
 
+  # return an array of strings containing every musician's name
   def all_artist_names
-    # return an array of strings containing every musician's name
+    artists.map(&:name).uniq
   end
 end
